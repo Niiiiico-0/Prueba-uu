2b0e9669a015cdad84fb3be8d1f782cae6e812d2
Niiiiico-0 <ra.avaria@duocuc.cl>
2026-06-03 19:04:11 -0400

    Ejercicio1
    ejercicio1 main

---
+++ ejercicio1.py
@@ -0,0 +1,19 @@
+notas = []
+suma = 0
+for contador in range(5):
+    while True:
+        try:
+            nota = float(input(f"Ingrese nota {contador + 1}:"))
+
+        except ValueError:
+            print("La nota debe ser un numero")
+        else:
+            if nota >=1 and nota <=7
+                suma += nota
+                nota.append(nota)
+                print("Nota registrada con exito")
+                break
+            else:
+                print("La nota debe estar entre 1 y 7")
+promedio = suma/len(notas)
+print(f"Promedio : {promedio}")
